8e20676d52cd78b2e0d0f0f49e502af05ad96f70
Dustin Miller <dmiller903@gmail.com>
2019-10-31 10:03:53 -0600

    change parallel processing

--- scripts/phase_with_shapeit_individual_trio.py
+++ scripts/phase_with_shapeit_individual_trio.py
@@ -3,30 +3,60 @@ import re
 import os
 import concurrent.futures
 import subprocess
+from sys import argv
 
-files = []
-for file in glob.glob("FM_YTPYPGH1_test/FM_YTPYPGH1_chr*.vcf"):
-    matches = re.findall(r"([\w\-\/]+)\.vcf", file)
-    files.append(matches[0])
+#Input file or list of files
+inputFile = argv[1]
+pathToFiles = argv[2]
+if pathToFiles.endswith("/"):
+    pathToFiles = pathToFiles[0:-1]
+#Create a list of file(s) that need to have unplaced and multiallelic sites removed
+fileDict = dict()
+if inputFile.endswith(".gz"):
+    fileDict.add(inputFile)
 
-with open("phase_with_shapeit_individual_trio_check.log", "w") as outputFile:
-    #def runShapeit(file):
-    for file in files:
-        chr = re.findall(r"[\w\-]+\/[\w\-]+(chr[0-9][0-9]?)", file)[0]
-        outputProcess = subprocess.Popen("/shapeit.v2.904.3.10.0-693.11.6.el7.x86_64/bin/shapeit -check -B {} --output-log {}.checks -M /references/ALL_1000G_phase1integrated_v3_impute_macGT1/genetic_map_{}_combined_b37.txt \
-        --input-ref /references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3.sample /references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3_{}_impute_macGT1.hap.gz \
-        /references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3_{}_impute_macGT1.legend.gz".format(file, file, chr, chr, chr), shell = True, stdout = subprocess.PIPE)
-        stdOut, stdErr = outputProcess.communicate()
-        stdOut = stdOut.decode("utf-8")
-        outputFile.write(stdOut)
-        stdErr = stdErr.decode("utf-8")
-        outputFile.write(stdErr)
+elif inputFile.endswith(".txt"):
+    with open(inputFile) as sampleFile:
+        for sample in sampleFile:
+            sample = sample.rstrip("\n")
+            fileDict.add(sample)
+
+elif inputFile.endswith(".tsv"):
+    with open(inputFile) as sampleFile:
+        header = sampleFile.readline()
+        headerList = header.rstrip().split("\t")
+        fileNameIndex = headerList.index("file_name")
+        familyIdIndex = headerList.index("family_id")
+        sampleIdIndex = headerList.index("sample_id")
+        chromosomes = {"chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13",\
+ "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"}
+        for sample in sampleFile:
+            sampleData = sample.rstrip("\n").split("\t")
+            fileName = sampleData[fileNameIndex]
+            sampleFamilyId = sampleData[familyIdIndex]
+            sampleId = sampleData[sampleIdIndex]
+            if sampleFamilyId not in fileDict:
+                fileDict[sampleFamilyId] = set()
+                for chromosome in chromosomes:
+                    #individualFileName = "{}/{}/{}/{}_{}".format(pathToFiles, sampleFamilyId, sampleId, sampleId, chromosome)
+                    trioFileName = "{}/{}/{}_trio/{}_trio_{}".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId, chromosome)
+                    #fileDict.add(individualFileName)
+                    fileDict[sampleFamilyId].add(trioFileName)
+
+for trio in fileDict:
+    trioChr = fileDict[trio]
+    def runShapeit(file):
+        chromosome = re.findall(r"[\w\-\/_]+(chr[0-9][0-9]?)", file)[0]
+        os.system("/shapeit.v2.904.2.6.32-696.18.7.el6.x86_64/bin/shapeit -check -B {} --output-log {} -M /references/1000GP_Phase3/genetic_map_{}_combined_b37.txt \
+        --input-ref /references/1000GP_Phase3/1000GP_Phase3_{}.hap.gz /references/1000GP_Phase3/1000GP_Phase3_{}.legend.gz \
+        /references/1000GP_Phase3/1000GP_Phase3.sample --thread 3".format(file, file, chromosome, chromosome, chromosome))
+        os.system("/shapeit.v2.904.2.6.32-696.18.7.el6.x86_64/bin/shapeit -B {} --output-log {} -O {}_phased -M /references/1000GP_Phase3/genetic_map_{}_combined_b37.txt \
+        --input-ref /references/1000GP_Phase3/1000GP_Phase3_{}.hap.gz /references/1000GP_Phase3/1000GP_Phase3_{}.legend.gz \
+        /references/1000GP_Phase3/1000GP_Phase3.sample --thread 3 --exclude-snp {}.snp.strand.exclude --force --no-mcmc".format(file, file, file, chromosome, chromosome, chromosome, file))
         #os.system("/shapeit.v2.904.3.10.0-693.11.6.el7.x86_64/bin/shapeit -B {} -M /references/ALL_1000G_phase1integrated_v3_impute_macGT1/genetic_map_{}_combined_b37.txt \
                 #--input-ref /references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3.sample /references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3_{}_impute_macGT1.hap.gz \
                 #/references/ALL_1000G_phase1integrated_v3_impute_macGT1/ALL_1000G_phase1integrated_v3_{}_impute_macGT1.legend.gz -O {}_phased --output-graph {}_phased.graph \
                 #--output-log {}_phased.log --exclude-snp /fslhome/dmill903/research/idiopathic_scoliosis/gVCF/FM_YTPYPGH1_test/FM_YTPYPGH1_chr21.checks.snp.strand.exclude".format(file, chr, chr, chr, file, file, file))
-        #os.system("shapeit -convert --input-haps {}.phased --output-vcf {}.phased.vcf".format(file, file))
-
-    #with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
-        #executor.map(runShapeit, files)
-        #executor.close()
+        os.system("/shapeit.v2.904.2.6.32-696.18.7.el6.x86_64/bin/shapeit -convert --input-haps {}_phased --output-vcf {}_phased.vcf".format(file, file))
+    with concurrent.futures.ProcessPoolExecutor(max_workers=23) as executor:
+        executor.map(runShapeit, trioChr)
--- scripts/separate_chr_generate_plink.py
+++ scripts/separate_chr_generate_plink.py
@@ -57,48 +57,78 @@ def separateByChr(file):
                 header = header + line
             elif not line.startswith("#") and line.split("\t")[0] not in chromosomeSet:
                 chromosomeNumber = line.split("\t")[0]
+                os.system("rm {}{}.*".format(outputName, chromosomeNumber))
+                os.system("rm {}{}*harmonized.*".format(outputName, chromosomeNumber))
+                os.system("rm {}{}*harmonized*.*".format(outputName, chromosomeNumber))
                 with open("{}{}.vcf".format(outputName, chromosomeNumber), "w") as chromosome:
                     chromosome.write(header)
                     chromosome.write(line)
-                    tmpFileSet.add("{}{}.vcf".format(outputName, chromosomeNumber))
                     chromosomeSet.add(chromosomeNumber)
+                    if "trio" in outputName:
+                        tmpFileSet.add("{}{}.vcf".format(outputName, chromosomeNumber))
             else:
                 with open("{}{}.vcf".format(outputName, chromosomeNumber), "a") as chromosome:
                     chromosome.write(line)
         return(tmpFileSet)
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=46) as executor:
+with concurrent.futures.ProcessPoolExecutor(max_workers=42) as executor:
     tmpFileList = executor.map(separateByChr, fileSet)
     for tmpList in tmpFileList:
         for tmpSet in tmpList:
             plinkFileSet.add(tmpSet)
 
+fileDict = dict()
+with open(inputFile) as sampleFile:
+        header = sampleFile.readline()
+        headerList = header.rstrip().split("\t")
+        fileNameIndex = headerList.index("file_name")
+        familyIdIndex = headerList.index("family_id")
+        sampleIdIndex = headerList.index("sample_id")
+        chromosomes = {"chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13",\
+ "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"}
+        for sample in sampleFile:
+            sampleData = sample.rstrip("\n").split("\t")
+            fileName = sampleData[fileNameIndex]
+            sampleFamilyId = sampleData[familyIdIndex]
+            sampleId = sampleData[sampleIdIndex]
+            if sampleFamilyId not in fileDict:
+                fileDict[sampleFamilyId] = set()
+                for chromosome in chromosomes:
+                    #individualFileName = "{}/{}/{}/{}_{}".format(pathToFiles, sampleFamilyId, sampleId, sampleId, chromosome)
+                    trioFileName = "{}/{}/{}_trio/{}_trio_{}.vcf".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId, chromosome)
+                    #fileDict.add(individualFileName)
+                    fileDict[sampleFamilyId].add(trioFileName)
 
 #Create harmonized bed, bim files for each chromosome
-def createPlink(file):
-    filePath, famFolder, sampleFolder, sampleFile, chrNumber = re.findall(r"([\w\-/_]+)\/([\w\-]+)\/([\w\-]+)\/([\w\-]+)_(chr[\w]+)\.?.*\.?.*\.vcf", file)[0]
-    outputName = "{}/{}/{}/{}_{}".format(filePath, famFolder, sampleFolder, sampleFile, chrNumber)
-    os.system("/plink2 --vcf {} --fam {}/{}/{}_trio.fam --make-bed --out {}".format(file, filePath, famFolder, famFolder, outputName))
-    if chrNumber[-1].isnumeric():
-        os.system("java -jar /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
-        --update-id \
-        -ura \
-        --outputType PLINK_BED --outpout {}_harmonized \
-        --refType VCF --ref /ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes".format(outputName, outputName, chrNumber))
-    elif chrNumber[-1] == "X":
-        os.system("java -jar /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
-        --update-id \
-        -ura \
-        --outputType PLINK_BED --outpout {}_harmonized \
-        --refType VCF --ref /ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes".format(outputName, outputName, chrNumber))
-    else:
-        os.system("java -jar /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
-        --update-id \
-        -ura \
-        --outputType PLINK_BED --outpout {}_harmonized \
-        --refType VCF --ref /ALL.chrY.phase3_integrated_v2a.20130502.genotypes".format(outputName, outputName, chrNumber))
-with concurrent.futures.ProcessPoolExecutor(max_workers=46) as executor:
-    executor.map(createPlink, plinkFileSet)
+for trio in fileDict:
+    trioChr = fileDict[trio]
+    def createPlink(file):
+        filePath, famFolder, sampleFolder, sampleFile, chrNumber = re.findall(r"([\w\-/_]+)\/([\w\-]+)\/([\w\-]+)\/([\w\-]+)_(chr[\w]+)\.?.*\.?.*\.vcf", file)[0]
+        outputName = "{}/{}/{}/{}_{}".format(filePath, famFolder, sampleFolder, sampleFile, chrNumber)
+        
+        os.system("/plink2 --vcf {} --fam {}/{}/{}_trio.fam --make-bed --out {}".format(file, filePath, famFolder, famFolder, outputName))
+
+        if chrNumber[-1].isnumeric():
+            os.system("java -jar -Xmx40g /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
+            --update-id \
+            -ura \
+            --outputType PLINK_BED --output {}_harmonized \
+            --refType VCF --ref /ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes".format(outputName, outputName, chrNumber))
+        elif chrNumber[-1] == "X":
+            os.system("java -jar -Xmx40g /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
+            --update-id \
+            -ura \
+            --outputType PLINK_BED --output {}_harmonized \
+            --refType VCF --ref /ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes".format(outputName, outputName, chrNumber))
+        else:
+            os.system("java -jar -Xmx40g /GenotypeHarmonizer-1.4.20-SNAPSHOT/GenotypeHarmonizer.jar --inputType PLINK_BED --input {} \
+            --update-id \
+            -ura \
+            --outputType PLINK_BED --output {}_harmonized \
+            --refType VCF --ref /ALL.chrY.phase3_integrated_v2a.20130502.genotypes".format(outputName, outputName, chrNumber))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=23) as executor:
+        executor.map(createPlink, trioChr)
 
 timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
 timeElapsedHours = round(timeElapsedMinutes / 60, 2)
