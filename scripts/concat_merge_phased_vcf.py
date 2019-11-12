21e01f34f1e40c9a77ada1161eb1d4cfc24ee4fa
Dustin Miller <dmiller903@gmail.com>
2019-11-12 11:32:34 -0700

    Script update

--- scripts/concat_merge_phased_vcf.py
+++ scripts/concat_merge_phased_vcf.py
@@ -10,8 +10,10 @@ inputFile = argv[1]
 pathToFiles = argv[2]
 if pathToFiles.endswith("/"):
     pathToFiles = pathToFiles[0:-1]
+
 #Create a list of file(s) that need to have unplaced and multiallelic sites removed
 fileDict = dict()
+concatFiles = list()
 if inputFile.endswith(".gz"):
     fileDict.add(inputFile)
 
@@ -37,22 +39,107 @@ elif inputFile.endswith(".tsv"):
             sampleId = sampleData[sampleIdIndex]
             if sampleFamilyId not in fileDict:
                 fileDict[sampleFamilyId] = list()
+                concatFileName = "{}/{}/{}_trio/{}_trio_phased_mcmc_combined.vcf.gz".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId)
+                concatFiles.append(concatFileName)
                 for chromosome in chromosomes:
                     #individualFileName = "{}/{}/{}/{}_{}".format(pathToFiles, sampleFamilyId, sampleId, sampleId, chromosome)
-                    trioFileName = "{}/{}/{}_trio/{}_trio_{}_phased.vcf".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId, chromosome)
+                    trioFileName = "{}/{}/{}_trio/{}_trio_{}_phased_mcmc_reverted.vcf".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId, chromosome)
                     #fileDict.add(individualFileName)
                     fileDict[sampleFamilyId].append(trioFileName)
 
-
 def concatMerge(trio):
     files = fileDict[trio]
+
     for index, file in enumerate(files):
-        os.system("bgzip {} && tabix -p vcf {}.gz".format(file, file))
+        #os.system("gzip -d {}.gz".format(file))
+        os.system("bgzip {} && tabix -fp vcf {}.gz".format(file, file))
         files[index] = "{}.gz".format(file)
-    fileName = re.findall(r"([\w\-\/_]+)_chr[A-Z0-9][A-Z0-9]?_phased\.vcf", files[0])[0]
-    outputName = "{}_phased_combined.vcf".format(fileName)
+
+
+    fileName = re.findall(r"([\w\-\/_]+\/[\w\-_]+)_chr[A-Z0-9][A-Z0-9]?_phased_mcmc_reverted\.vcf", files[0])[0]
+    outputName = "{}_phased_mcmc_combined.vcf".format(fileName)
     files = " ".join(files)
     os.system("bcftools concat {} -o {}".format(files, outputName))
-    
-with concurrent.futures.ProcessPoolExecutor(max_workers=23) as executor:
+    os.system("bgzip -f {} && tabix -fp vcf {}.gz".format(outputName, outputName))
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=35) as executor:
     executor.map(concatMerge, fileDict)
+
+# Merge all phased, concatenated, trio files into one    
+concatFilesString = " ".join(concatFiles)
+outputName = "{}/idopathic_scoliosis_phased_mcmc_samples.vcf".format(pathToFiles)
+os.system("bcftools merge -m both {} -o {}".format(concatFilesString, outputName))
+os.system("bgzip -f {} && tabix -fp vcf {}.gz".format(outputName, outputName))
+
+# Create a merged family file
+# create a proband dictionary where the key is the sampleId and the value is the familyId
+# also create a parent dictionary where the key is familyId and the value is a dictionary that has a key of the sampleId and value of gender
+probandDict = {}
+parentDict = {}
+with open(inputFile) as sampleFile:
+    header = sampleFile.readline()
+    headerList = header.rstrip().split("\t")
+    fileNameIndex = headerList.index("file_name")
+    familyIdIndex = headerList.index("family_id")
+    sampleIdIndex = headerList.index("sample_id")
+    probandIndex = headerList.index("proband")
+    genderIndex = headerList.index("sex")
+    for sample in sampleFile:
+        sampleData = sample.rstrip("\n").split("\t")
+        fileName = sampleData[fileNameIndex]
+        sampleFamilyId = sampleData[familyIdIndex]
+        sampleId = sampleData[sampleIdIndex]
+        probandStatus = sampleData[probandIndex]
+        gender = sampleData[genderIndex]
+        if probandStatus == "Yes":
+            probandDict[sampleId] = sampleFamilyId
+        else:
+            if sampleFamilyId not in parentDict:
+                parentDict[sampleFamilyId] = {sampleId: gender}
+            else:
+                parentDict[sampleFamilyId][sampleId] = gender
+
+# Create a dictionary where each sample has the rest of the family information needed for the family file
+sampleDict = dict()
+with open(inputFile) as sampleFile:
+    header = sampleFile.readline()
+    headerList = header.rstrip().split("\t")
+    fileNameIndex = headerList.index("file_name")
+    familyIdIndex = headerList.index("family_id")
+    sampleIdIndex = headerList.index("sample_id")
+    probandIndex = headerList.index("proband")
+    genderIndex = headerList.index("sex")
+    for sample in sampleFile:
+        sampleData = sample.rstrip("\n").split("\t")
+        fileName = sampleData[fileNameIndex]
+        sampleFamilyId = sampleData[familyIdIndex]
+        sampleId = sampleData[sampleIdIndex]
+        probandStatus = sampleData[probandIndex]
+        gender = sampleData[genderIndex]
+        paternal = ""
+        maternal = ""
+        if probandStatus == "Yes":
+            familyDict = parentDict[sampleFamilyId]
+            for key, value in familyDict.items():
+                if value == "1":
+                    paternal = key
+                else:
+                    maternal = key
+            sampleDict[sampleId] = "{}\t{}\t{}\t{}\t{}\t2\n".format(sampleFamilyId, sampleId, paternal, maternal, gender)
+        else:
+            sampleDict[sampleId] = "{}\t{}\t0\t0\t{}\t1\n".format(sampleFamilyId, sampleId, gender)
+            
+# create a sample list in the order of the vcf file
+with open("{}/idopathic_scoliosis_phased_mcmc_samples.vcf".format(pathToFiles)) as vcfFile:
+    for line in vcfFile:
+        if line.startswith("##"):
+            continue
+        elif line.startswith("#CHROM"):
+            sampleList = line.rstrip().split("\t")[9:]
+        else:
+            break
+
+# use the sample order in the list to output each sample in order as found in the vcf file
+with open("{}/idopathic_scoliosis.fam".format(pathToFiles), "w") as outputFile:
+    for sample in sampleList:
+        outputFile.write(sampleDict[sample])
