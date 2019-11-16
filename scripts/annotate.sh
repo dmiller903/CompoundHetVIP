0c11bb4e5c3a263c7dc0e000f45041b51281a4d3
Dustin Miller <dmiller903@gmail.com>
2019-11-15 17:40:11 -0700

    remove dbNSFP annotation step

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#java -Xmx40g -jar /snpEff/snpEff.jar GRCh37.75 -v \
-	#idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_vt.vcf > \
-	#idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated.vcf
-
-java -Xmx40g -jar /snpEff/SnpSift.jar dbNSFP -v \
-	-db /snpEff/./data/dbNSFP2.9.txt.gz \
-	idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated.vcf > \
-	idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated_dbNSFP.vcf
+java -Xmx40g -jar /snpEff/snpEff.jar GRCh37.75 -v \
+	idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_vt.vcf > \
+	idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated.vcf
