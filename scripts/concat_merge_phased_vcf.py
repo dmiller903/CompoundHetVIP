fe5ca4bf51399ddcbc336932189fbf544fde8d36
Dustin Miller <dmiller903@gmail.com>
2019-11-15 17:49:20 -0700

    file misspelling fix

--- scripts/concat_merge_phased_vcf.py
+++ scripts/concat_merge_phased_vcf.py
@@ -69,7 +69,7 @@ with concurrent.futures.ProcessPoolExecutor(max_workers=35) as executor:
 
 # Merge all phased, concatenated, trio files into one    
 concatFilesString = " ".join(concatFiles)
-outputName = "{}/idopathic_scoliosis_phased_samples.vcf".format(pathToFiles)
+outputName = "{}/idiopathic_scoliosis_phased_samples.vcf".format(pathToFiles)
 os.system("bcftools merge -m both {} -o {}".format(concatFilesString, outputName))
 os.system("bgzip -f {} && tabix -fp vcf {}.gz".format(outputName, outputName))
 
@@ -132,7 +132,7 @@ with open(inputFile) as sampleFile:
             sampleDict[sampleId] = "{}\t{}\t0\t0\t{}\t1\n".format(sampleFamilyId, sampleId, gender)
             
 # create a sample list in the order of the vcf file
-with gzip.open("{}/idopathic_scoliosis_phased_samples.vcf.gz".format(pathToFiles), "rt") as vcfFile:
+with gzip.open("{}/idiopathic_scoliosis_phased_samples.vcf.gz".format(pathToFiles), "rt") as vcfFile:
     for line in vcfFile:
         if line.startswith("##"):
             continue
@@ -142,6 +142,6 @@ with gzip.open("{}/idopathic_scoliosis_phased_samples.vcf.gz".format(pathToFiles
             break
 
 # use the sample order in the list to output each sample in order as found in the vcf file
-with open("{}/idopathic_scoliosis.fam".format(pathToFiles), "w") as outputFile:
+with open("{}/idiopathic_scoliosis.fam".format(pathToFiles), "w") as outputFile:
     for sample in sampleList:
         outputFile.write(sampleDict[sample])
--- scripts/gemini_query.sh
+++ scripts/gemini_query.sh
@@ -2,5 +2,5 @@
 
 gemini comp_hets --columns "chrom, gene, start, end, impact_severity, aaf_1kg_all, cadd_scaled, impact" \
 --filter "(impact_severity = 'HIGH' or is_lof = 1) or (impact_severity = 'MED' and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
-idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated_cadd.db \
-> idiopathic_scoliosis/gVCF/idopathic_scoliosis_ch_high_med_aaf_cadd.tsv
+idiopathic_scoliosis/gVCF/idiopathic_scoliosis_phased_samples_annotated_cadd.db \
+> idiopathic_scoliosis/gVCF/idiopathic_scoliosis_ch_high_med_aaf_cadd.tsv
