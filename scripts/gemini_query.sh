84077255dcd2b6700831933a0146934057031d79
Dustin Miller <dmiller903@gmail.com>
2019-11-15 17:44:16 -0700

    update filter criteria

--- scripts/gemini_query.sh
+++ scripts/gemini_query.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-gemini comp_hets --columns "chrom, gene, start, end, impact_severity" \
---filter "impact_severity = 'HIGH'" \
-idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated.db \
-> idiopathic_scoliosis/gVCF/idopathic_scoliosis_ch_high.tsv
-
-
-#gemini comp_hets --columns "chrom, gene, start, end, impact_severity" \
-#--filter "impact_severity = 'HIGH' or impact_severity = 'MED'" \
-#idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated.db \
-#> idiopathic_scoliosis/gVCF/idopathic_scoliosis_ch_high_med.tsv
+gemini comp_hets --columns "chrom, gene, start, end, impact_severity, aaf_1kg_all, cadd_scaled, impact" \
+--filter "(impact_severity = 'HIGH' or is_lof = 1) or (impact_severity = 'MED' and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
+idiopathic_scoliosis/gVCF/idopathic_scoliosis_phased_samples_annotated_cadd.db \
+> idiopathic_scoliosis/gVCF/idopathic_scoliosis_ch_high_med_aaf_cadd.tsv
