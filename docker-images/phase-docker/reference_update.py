e4429d4de52c64500f9a348201bf40b0eb79fb99
Dustin Miller <dmiller903@gmail.com>
2020-01-29 13:57:13 -0700

    reduce number of workers

--- docker-images/phase-docker/reference_update.py
+++ docker-images/phase-docker/reference_update.py
@@ -27,5 +27,5 @@ def updateFiles(file):
     os.system(f"wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr{chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.tbi \
     && mv ALL.chr{chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.tbi /references/1000GP_Phase3/")
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+with concurrent.futures.ProcessPoolExecutor(max_workers=11) as executor:
     executor.map(updateFiles, glob.glob("/references/1000GP_Phase3/genetic_map_chr*_combined_b37.txt"))
