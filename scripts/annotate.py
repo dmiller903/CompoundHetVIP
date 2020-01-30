b057a1487bbf9d495e459814b6a6965c2e3dc384
Dustin Miller <dmiller903@gmail.com>
2020-01-30 16:53:06 -0700

    indentation fix

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -11,5 +11,5 @@ diseaseName = chromosome = re.findall(r"([\w\-_]+)\/", pathToFiles)[0]
 
 # Annotate the vt trimmed file
 os.system(f"java -Xmx40g -jar /snpEff/snpEff.jar GRCh37.75 -v \
-	{pathToFiles}/{diseaseName}_phased_samples_vt.vcf > \
-	{pathToFiles}/{diseaseName}_phased_samples_annotated.vcf")
+{pathToFiles}/{diseaseName}_phased_samples_vt.vcf > \
+{pathToFiles}/{diseaseName}_phased_samples_annotated.vcf")
