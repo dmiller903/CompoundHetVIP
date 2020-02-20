31efaac27ea8826b2ffcd0a1a830220a4e217bf9
Dustin Miller <dmiller903@gmail.com>
2020-02-20 16:43:13 -0700

    add query for cadd >= 15

--- scripts/gemini_query.py
+++ scripts/gemini_query.py
@@ -11,43 +11,55 @@ diseaseName = chromosome = re.findall(r"([\w\-_]+)\/", pathToFiles)[0]
 med = "'MED'"
 high = "'HIGH'"
 
-# Filter CH Variants with minor allele frequency <= 0.005
+# Filter CH Variants with minor allele frequency <= 0.005 and cadd >= 20
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
 > {pathToFiles}/{diseaseName}_ch_impactHM_aaf005_cadd20.tsv')
 
-# Filter CH Variants with minor allele frequency <= 0.01
+# Filter CH Variants with minor allele frequency <= 0.01 and cadd >= 20
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
 > {pathToFiles}/{diseaseName}_ch_impactHM_aaf01_cadd20.tsv')
 
-# Filter CH Variants with no regard for minor allele frequency
+# Filter CH Variants with no regard for minor allele frequency and cadd >= 20
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
 > {pathToFiles}/{diseaseName}_ch_impactHM_cadd20.tsv')
 
-# Filter CH Variants with CADD score >= 20
+# Filter CH Variants with minor allele frequency <= 0.005 and cadd >= 15
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "cadd_scaled >=20" \
+--filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=15)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
-> {pathToFiles}/{diseaseName}_ch_cadd20.tsv')
+> {pathToFiles}/{diseaseName}_ch_impactHM_aaf005_cadd15.tsv')
 
-# Filter for de novo variants with minor allele frequncey <= 0.005
+# Filter CH Variants with minor allele frequency <= 0.01 and cadd >= 15
+os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
+--filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=15)" \
+{pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
+> {pathToFiles}/{diseaseName}_ch_impactHM_aaf01_cadd15.tsv')
+
+# Filter CH Variants with no regard for minor allele frequency and cadd >= 15
+os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
+--filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=15)" \
+{pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
+> {pathToFiles}/{diseaseName}_ch_impactHM_cadd15.tsv')
+
+# Filter for de novo variants with minor allele frequncey <= 0.005 and cadd >= 20
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
 > {pathToFiles}/{diseaseName}_de_novo_impactHM_aaf005_cadd20.tsv')
 
-# Filter for de novo variants with minor allele frequncey <= 0.01
+# Filter for de novo variants with minor allele frequncey <= 0.01 and cadd >= 20
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
 > {pathToFiles}/{diseaseName}_de_novo_impactHM_aaf01_cadd20.tsv')
 
-# Filter for de novo variants with no regard for minor allele frequency
+# Filter for de novo variants with no regard for minor allele frequency and cadd >= 20
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=20)" \
 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db \
