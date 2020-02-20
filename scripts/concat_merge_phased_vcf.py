e9b954609d84a554b3e46b2f9d2942741b7598d6
Dustin Miller <dmiller903@gmail.com>
2020-02-19 18:13:32 -0700

    begin to generalize

--- scripts/concat_merge_phased_vcf.py
+++ scripts/concat_merge_phased_vcf.py
@@ -1,54 +1,32 @@
-import glob
-import re
 import os
-import concurrent.futures
-import subprocess
-from sys import argv
+import time
+import argparse
+import re
 import gzip
+import glob
+#Keep track of when the script began
+startTime = time.time()
+char = '\n' + ('*' * 70) + '\n'
 
-#Input file or list of files
-inputFile = argv[1]
-pathToFiles = argv[2]
-if pathToFiles.endswith("/"):
-    pathToFiles = pathToFiles[0:-1]
-diseaseName = re.findall(r"[\w_\-]+", pathToFiles)[0]
+# Argparse Information
+parser = argparse.ArgumentParser(description="Phased results can have the REF and ALT alleles switched as compared \
+to the reference genome. We are unsure exactly why this occurs. For files with trios, it may be that since each file \
+only has 3 samples, the ALT allele is more common in the trio and becomes the REF. This step ensures that the REF/ALT \
+alleles of the phased VCF files are congruent with the REF/ALT of the reference genome. In addition, sites with Mendel \
+errors are removed.")
 
-#Create a list of file(s) that need to have unplaced and multiallelic sites removed
-fileDict = dict()
-concatFiles = list()
-if inputFile.endswith(".gz"):
-    fileDict.add(inputFile)
+parser.add_argument('input_vcf', help='Input file')
+parser.add_argument('output_file', help='Name of output file')
+parser.add_argument('chromosome_number', help='Chromosome number is needed so the script can determine which reference \
+file to use.')
 
-elif inputFile.endswith(".txt"):
-    with open(inputFile) as sampleFile:
-        for sample in sampleFile:
-            sample = sample.rstrip("\n")
-            fileDict.add(sample)
+args = parser.parse_args()
 
-elif inputFile.endswith(".tsv"):
-    with open(inputFile) as sampleFile:
-        header = sampleFile.readline()
-        headerList = header.rstrip().split("\t")
-        fileNameIndex = headerList.index("file_name")
-        familyIdIndex = headerList.index("family_id")
-        sampleIdIndex = headerList.index("sample_id")
-        chromosomes = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13",\
- "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
-        for sample in sampleFile:
-            sampleData = sample.rstrip("\n").split("\t")
-            fileName = sampleData[fileNameIndex]
-            sampleFamilyId = sampleData[familyIdIndex]
-            sampleId = sampleData[sampleIdIndex]
-            if sampleFamilyId not in fileDict:
-                fileDict[sampleFamilyId] = list()
-                # concatFileName is what each trio will be named after each trio is combined into one trio
-                concatFileName = "{}/{}/{}_trio/{}_trio_phased_combined.vcf.gz".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId)
-                concatFiles.append(concatFileName)
-                for chromosome in chromosomes:
-                    #individualFileName = "{}/{}/{}/{}_{}".format(pathToFiles, sampleFamilyId, sampleId, sampleId, chromosome)
-                    trioFileName = "{}/{}/{}_trio/{}_trio_{}_phased_reverted.vcf".format(pathToFiles, sampleFamilyId, sampleFamilyId, sampleFamilyId, chromosome)
-                    #fileDict.add(individualFileName)
-                    fileDict[sampleFamilyId].append(trioFileName)
+#Create variables of each argument from argparse
+inputFile = args.input_vcf
+outputFile = args.output_file
+chromosome = args.chromosome_number
+tempFile = "/tmp/" + re.findall(r'/?([\w\-_\.]+)', outputFile)[-1]
 
 def concatMerge(trio):
     files = fileDict[trio]
