339a5724ecb4a9c362203b831719ccd59030ea80
Dustin Miller <dmiller903@gmail.com>
2020-03-03 14:16:45 -0700

    update comments and descriptions

--- scripts/keep_variant_sites.py
+++ scripts/keep_variant_sites.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import gzip
 import re
 import os
@@ -10,27 +11,30 @@ startTime = time.time()
 char = '\n' + ('*' * 70) + '\n'
 
 # Argparse Information
-parser = argparse.ArgumentParser(description='gVCF files are different from VCF files in that they contain information \
-for every position, including non-variant positions. Therefore, these files are large and would take a long time to \
-process if the non-variant positions were included throughout the whole compound heterozygous pipeline. "keep_variant_sites.py" takes \
-each sample file and removes all non-variant sites. If parental files are included, the script then filters each parent \
-file for sites that only occur in the affected sample of that family.')
+parser = argparse.ArgumentParser(description='If input files are VCF and no parent files are available, then this script \
+does not need to be used. However, if files are VCF files and parent files are available, the script will filter each \
+parent gVCF file for sites that only occur in the child of that family. The optional parameter, "--is_gvcf" needs to be \
+changed to "n". gVCF files are different from VCF files in that they contain information for every position, including \
+non-variant positions. Therefore, these files are large and would take a long time to process if the non-variant \
+positions were included throughout the whole compound heterozygous pipeline. "keep_variant_sites.py" takes each sample \
+file and removes all non-variant sites. If parental files are included, the script then filters each parent file for \
+sites that only occur in the affected sample of that family.')
 
-parser.add_argument('sample_gVCF', help='Sample File')
-parser.add_argument('output_path', help='Path to where output files should go')
-parser.add_argument('--parent_1_gVCF', help='Maternal or Paternal File of Sample')
-parser.add_argument('--parent_2_gVCF', help='Maternal or Paternal File of Sample')
+parser.add_argument('sample_file', help='Sample File')
+parser.add_argument('output_path', help='Path to where output file(s) should go')
+parser.add_argument('--parent_1_file', help='Maternal or Paternal File of Sample')
+parser.add_argument('--parent_2_file', help='Maternal or Paternal File of Sample')
 parser.add_argument('--output_suffix', help='Suffix for each output file (do not include .gz at end as this will be included \
-when the file is bgzipped', default='_parsed.vcf')
+when the file is bgzipped)', default='_parsed.vcf')
 parser.add_argument('--is_gvcf', help='If a gVCF file is used, all non-variant sites will be filtered out of the sample file \
 and a new VCF will be created', default='y')
 
 args = parser.parse_args()
 
 #Create variables of each argument from argparse
-sampleFile = args.sample_gVCF
-parent1File = args.parent_1_gVCF
-parent2File = args.parent_2_gVCF
+sampleFile = args.sample_file
+parent1File = args.parent_1_file
+parent2File = args.parent_2_file
 outputPath = args.output_path
 isGvcf = args.is_gvcf
 if outputPath.endswith("/"):
@@ -39,7 +43,7 @@ outputSuffix = args.output_suffix
 
 #Filter each sample file, remove  variants-only sites, create a dictionary of variant-only sites
 fileName = re.findall(r'/?([\w\-_]+)\.?g?\.vcf\.gz', sampleFile)[0]
-outputName = "{}/{}{}".format(outputPath, fileName, outputSuffix)
+outputName = f"{outputPath}/{fileName}{outputSuffix}"
 positionDict = {}
 if isGvcf == "y":
     with gzip.open(sampleFile, 'rt') as gVCF, gzip.open(outputName, 'wb') as parsed:
@@ -56,13 +60,14 @@ if isGvcf == "y":
                 else:
                     positionDict[chrom].add(pos)
     #bgzip file
-    os.system("zcat {} | /root/miniconda2/bin/bgzip > {}.gz".format(outputName, outputName))
+    os.system(f"zcat {outputName} | /root/miniconda2/bin/bgzip > {outputName}.gz")
     os.system(f"rm {outputName}")
 
     #Print message and how long the previous steps took
     timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
     timeElapsedHours = round(timeElapsedMinutes / 60, 2)
-    print('Non-variant sites have been removed from sample file. Time elapsed: {} minutes ({} hours)'.format(timeElapsedMinutes, timeElapsedHours))
+    print(f'Non-variant sites have been removed from sample file. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours)')
+
 elif isGvcf == "n":
     with gzip.open(sampleFile, 'rt') as gVCF:
         for line in gVCF:
@@ -77,36 +82,34 @@ elif isGvcf == "n":
                 else:
                     positionDict[chrom].add(pos)
 
-
 #Filter each parent file for sites that occur in sample of that family
-if parent1File != None and parent2File != None:
-    def filterParents(file):
-        fileName = re.findall(r'/?([\w\-_]+)\.?g?\.vcf\.gz', file)[0]
-        outputName = "{}/{}{}".format(outputPath, fileName, outputSuffix)
-        with gzip.open(file, 'rt') as gVCF, gzip.open(outputName, 'wb') as parsed:
-            for line in gVCF:
-                if line.startswith("#"):
+def filterParents(file):
+    fileName = re.findall(r'/?([\w\-_]+)\.?g?\.vcf\.gz', file)[0]
+    outputName = f"{outputPath}/{fileName}{outputSuffix}"
+    with gzip.open(file, 'rt') as gVCF, gzip.open(outputName, 'wb') as parsed:
+        for line in gVCF:
+            if line.startswith("#"):
+                parsed.write(line.encode())
+            else:
+                lineList = line.split("\t")
+                chrom = lineList[0]
+                pos = lineList[1]
+                if pos in positionDict[chrom]:
                     parsed.write(line.encode())
                 else:
-                    lineList = line.split("\t")
-                    chrom = lineList[0]
-                    pos = lineList[1]
-                    if pos in positionDict[chrom]:
-                        parsed.write(line.encode())
-                    else:
-                        if "END" in line:
-                            for i in range(int(pos), int(lineList[7].lstrip("END=")) + 1):
-                                if str(i) in positionDict[chrom]:
-                                    parsed.write(line.encode())
-        #bgzip file
-        os.system("zcat {} | /root/miniconda2/bin/bgzip > {}.gz".format(outputName, outputName))
-        os.system(f"rm {outputName}")
+                    if "END" in line:
+                        for i in range(int(pos), int(lineList[7].lstrip("END=")) + 1):
+                            if str(i) in positionDict[chrom]:
+                                parsed.write(line.encode())
+    #bgzip file
+    os.system(f"zcat {outputName} | /root/miniconda2/bin/bgzip > {outputName}.gz")
+    os.system(f"rm {outputName}")
 
+if parent1File != None and parent2File != None:
     with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
         executor.map(filterParents, [parent1File, parent2File])
 
-    #Print message and how long the previous steps took
-    timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
-    timeElapsedHours = round(timeElapsedMinutes / 60, 2)
-    print('Sites not corresponding to sample file have been removed for each parent. Time elapsed: {} minutes ({} hours)'.format(timeElapsedMinutes, timeElapsedHours))
-print("Done!")
+#Print message and how long the previous steps took
+timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
+timeElapsedHours = round(timeElapsedMinutes / 60, 2)
+print(f'Sites not corresponding to sample file have been removed for each parent. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours)')
