de08b8feadb3ad9a91593eaef48092bb4bf726f6
Dustin Miller <dmiller903@gmail.com>
2020-03-03 15:22:14 -0700

    download reference files

--- scripts/combine_trio.py
+++ scripts/combine_trio.py
@@ -1,6 +1,8 @@
+#  Import necessary modules
 import os
 import time
 import argparse
+import glob
 
 #Keep track of when the script began
 startTime = time.time()
@@ -8,7 +10,7 @@ char = '\n' + ('*' * 70) + '\n'
 
 # Argparse Information
 parser = argparse.ArgumentParser(description='Creates a combined trio file using GATKs "CombineGVCFs" and GenotypeGVCFs \
-tools')
+tools if gVCF files are used. If VCF files are used, the trio will be combined with bcftools')
 
 parser.add_argument('proband_vcf', help='Proband VCF File')
 parser.add_argument('parent_1_vcf', help='Maternal or Paternal VCF File of Proband')
@@ -26,25 +28,40 @@ parent2File = args.parent_2_vcf
 outputName = args.output_vcf
 isGvcf = args.is_gvcf
 
+# Download reference files if needed
+if not os.path.exists("/references/Homo_sapiens_assembly38.fasta") and isGvcf == "y":
+    os.system("wget --no-check-certificate \
+    https://files.osf.io/v1/resources/3znuj/providers/osfstorage/5d9f54d2a7bc73000ee99fd6/?zip= -O /tmp/references.zip \
+    && unzip /tmp/references.zip -d /tmp/references \
+    && rm /tmp/references.zip \
+    && gzip -d /tmp/references/*.gz")
+    for file in glob.glob("/tmp/references/*"):
+        fileName = file.split("/")[-1]
+        if not os.path.exists(f"/references/{fileName}"):
+            os.system(f"mv {file} /references/")
+
 # Use GATK to combine all trios into one vcf and then genotype the combined trio vcf
 files = [probandFile, parent1File, parent2File]
 tempName = "/tmp/temp.vcf"
 if isGvcf == "y":
-    fileString = ""
-    for file in files:
-        fileString += "-V {} ".format(file)
-        os.system("/root/miniconda2/bin/gatk IndexFeatureFile -F {}".format(file))
-    os.system("/root/miniconda2/bin/gatk CombineGVCFs -R /references/Homo_sapiens_assembly38.fasta {} -O {}".format(fileString, tempName))
-    os.system("gatk IndexFeatureFile -F {}".format(tempName))
-    os.system('gatk --java-options "-Xmx4g" GenotypeGVCFs -R /references/Homo_sapiens_assembly38.fasta -V {} -O {}'.format(tempName, outputName))
+    try:
+        fileString = ""
+        for file in files:
+            fileString += f"-V {file} "
+            os.system(f"/root/miniconda2/bin/gatk IndexFeatureFile -F {file}")
+        os.system(f"/root/miniconda2/bin/gatk CombineGVCFs -R /references/Homo_sapiens_assembly38.fasta {fileString} -O {tempName}")
+        os.system(f"gatk IndexFeatureFile -F {tempName}")
+        os.system(f"gatk --java-options '-Xmx4g' GenotypeGVCFs -R /references/Homo_sapiens_assembly38.fasta -V {tempName} -O {outputName}")
+    except:
+        print("Trio not combined, there was an error detected by GATK")
 
 elif isGvcf == "n":
     for file in files:
-        os.system("tabix -fp vcf {}".format(file))
+        os.system(f"tabix -fp vcf {file}")
     fileString = " ".join(files)
-    os.system("bcftools merge {} -o {} && tabix -fp vcf {}".format(fileString, outputName, outputName))
+    os.system(f"bcftools merge {fileString} -o {outputName} && tabix -fp vcf {outputName}")
 
 #Print message and how long the previous steps took
 timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
 timeElapsedHours = round(timeElapsedMinutes / 60, 2)
-print('{}Trio has been combined. Time elapsed: {} minutes ({} hours){}'.format(char, timeElapsedMinutes, timeElapsedHours, char))
+print(f'{char}Done. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours){char}')
