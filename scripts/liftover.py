6f28d6c0ce6b57414dc456fdb3c628e778540445
Dustin Miller <dmiller903@gmail.com>
2020-03-03 16:15:27 -0700

    download reference files if necessary

--- scripts/liftover.py
+++ scripts/liftover.py
@@ -1,16 +1,18 @@
+# Import necessary modules
 import os
 import time
 import argparse
 import re
+import glob
 
-#Keep track of when the script began
+# Keep track of when the script began
 startTime = time.time()
 char = '\n' + ('*' * 70) + '\n'
 
 # Argparse Information
 parser = argparse.ArgumentParser(description='Some phasing programs and other programs such as GEMINI, require that VCF \
-files be aligned to GRCh37. Therefore, this step takes an input VCF that is aligned to GRCh38 converts it to GRCh37 \
-positions file using GATKs "CombineGVCFs" tool')
+ or gVCF files be aligned to GRCh37. Therefore, this step takes an input VCF/gVCF that is aligned to GRCh38 converts it \
+to GRCh37 using GATKs "CombineGVCFs" tool')
 
 parser.add_argument('input_vcf', help='Input VCF file')
 parser.add_argument('output_vcf', help='Path and name of output VCF file. Do not include ".gz" at the end of the file \
@@ -18,16 +20,35 @@ name as this will be included during liftover')
 
 args = parser.parse_args()
 
-#Create variables of each argument from argparse
+# Create variables of each argument from argparse
 inputFile = args.input_vcf
 outputFile = args.output_vcf
 fileWithoutSuffix = re.findall(r'([\w\-_/]+)\.', outputFile)[0]
 
-#Liftover file(s)
-os.system("java -Xmx12G -jar /root/miniconda2/share/picard-2.21.1-0/picard.jar LiftoverVcf I={} O={}.gz \
-CHAIN=/references/hg38ToHg19.over.chain R=/references/human_g1k_v37_modified.fasta REJECT={}_rejected_variants.vcf".format(inputFile, outputFile, fileWithoutSuffix))
+# Download necessary reference files if needed
+if not os.path.exists("/references/hg38ToHg19.over.chain"):
+    os.system("wget --no-check-certificate \
+        https://files.osf.io/v1/resources/3znuj/providers/osfstorage/5d9ddd0ba7bc73000ce87e38/?zip= -O /tmp/references.zip \
+        && unzip /tmp/references.zip -d /tmp/references \
+        && rm /tmp/references.zip \
+        && /root/miniconda2/bin/bgzip -d /tmp/references/human_g1k_v37_modified.fasta.gz \
+        && /root/miniconda2/bin/bgzip -d /tmp/references/hg38ToHg19.over.chain.gz \
+        && /root/miniconda2/bin/bgzip -d /tmp/references/Homo_sapiens_assembly38.fasta.gz \
+        && /root/miniconda2/bin/bgzip -d /tmp/references/Homo_sapiens_assembly38.dict.gz \
+        && /root/miniconda2/bin/bgzip -d /tmp/references/Homo_sapiens_assembly38.fasta.fai.gz")
+    for file in glob.glob("/tmp/references/*"):
+        fileName = file.split("/")[-1]
+        if not os.path.exists(f"/references/{fileName}"):
+            os.system(f"mv {file} /references/")
+        elif fileName == "readme":
+            os.system(f"mv {file} /references/")
+    os.system("chmod 777 /references/*")
 
-#Output message and time complete
+# Liftover file(s)
+os.system(f"java -Xmx12G -jar /root/miniconda2/share/picard-2.21.1-0/picard.jar LiftoverVcf I={inputFile} O={outputFile}.gz \
+CHAIN=/references/hg38ToHg19.over.chain R=/references/human_g1k_v37_modified.fasta REJECT={fileWithoutSuffix}_rejected_variants.vcf")
+
+# Output message and time complete
 timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
 timeElapsedHours = round(timeElapsedMinutes / 60, 2)
-print('{}Liftover Complete. Time elapsed: {} minutes ({} hours){}'.format(char, timeElapsedMinutes, timeElapsedHours, char))
+print(f'{char}Done. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours){char}')
