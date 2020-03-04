0b7c6f2f90cd5684cee514a1a8f2d17f0252b1cd
Dustin Miller <dmiller903@gmail.com>
2020-03-04 11:14:45 -0700

    update syntax and comments

--- scripts/separate_chr_generate_plink.py
+++ scripts/separate_chr_generate_plink.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import os
 import time
 import argparse
@@ -17,7 +18,7 @@ chromosome VCF files. This step also generates the necessary PLINK files needed 
 parser.add_argument('input_vcf', help='Input VCF file')
 parser.add_argument('output_file', help='Path and prefix name of output files (no suffix). e.g. "/Data/file1"')
 parser.add_argument('--fam_file', help='If using a trio, a fam file is needed to create appropriate PLINK files. \
-If no fam file is included, PLINK with output a generic fam file. \
+If no fam file is included, PLINK will output a generic fam file. \
 see https://www.cog-genomics.org/plink/2.0/formats#fam for formatting guidelines')
 
 args = parser.parse_args()
@@ -30,7 +31,7 @@ famFile = args.fam_file
 #Separate combined trio files and individual participant files by chromosome
 plinkFileSet = set()
 with gzip.open(inputFile, "rt") as vcf:
-    outputName = "{}_".format(outputFile)
+    outputName = f"{outputFile}_"
     chromosomeSet = set()
     chromosomeNumber = ""
     header = ""
@@ -40,15 +41,15 @@ with gzip.open(inputFile, "rt") as vcf:
         elif not line.startswith("#") and line.split("\t")[0] not in chromosomeSet:
             chromosomeNumber = line.split("\t")[0]
             if (chromosomeNumber[3:].isnumeric() and int(chromosomeNumber[3:]) in range(0,23)) or (chromosomeNumber[3:].isalpha() and str(chromosomeNumber[3:]) in ["X", "Y"]):
-                with gzip.open("{}{}.vcf.gz".format(outputName, chromosomeNumber), "wb") as chromosome:
+                with gzip.open(f"{outputName}{chromosomeNumber}.vcf.gz", "wb") as chromosome:
                     chromosome.write(header.encode())
                     chromosome.write(line.encode())
                     chromosomeSet.add(chromosomeNumber)
-                    plinkFileSet.add("{}{}.vcf.gz".format(outputName, chromosomeNumber))
+                    plinkFileSet.add(f"{outputName}{chromosomeNumber}.vcf.gz")
             else:
                 break
         else:
-            with gzip.open("{}{}.vcf.gz".format(outputName, chromosomeNumber), "ab") as chromosome:
+            with gzip.open(f"{outputName}{chromosomeNumber}.vcf.gz", "ab") as chromosome:
                 chromosome.write(line.encode())
 
 #Create bed, bim files for each chromosome of each trio
@@ -57,10 +58,11 @@ plinkFileList.sort()
 for file in plinkFileList:
     outputName = re.findall(r"([\w/_\-]+chr[0-9XY][0-9XY]?)", file)[0]
     if famFile != None:
-        os.system("/plink2 --vcf {} --fam {} --make-bed --out {}".format(file, famFile, outputName))
+        os.system(f"/plink2 --vcf {file} --fam {famFile} --make-bed --out {outputName}")
     else:
-        os.system("/plink2 --vcf {} --make-bed --out {}".format(file, outputName))
+        os.system(f"/plink2 --vcf {file} --make-bed --out {outputName}")
 
+# Output Time information
 timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
 timeElapsedHours = round(timeElapsedMinutes / 60, 2)
-print('{}File has been separated by chromosome. Time elapsed: {} minutes ({} hours){}'.format(char, timeElapsedMinutes, timeElapsedHours, char))
+print(f'{char}Done. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours){char}')
