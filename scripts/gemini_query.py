6a2af9bce886978df03e8ed00b43c4a75c6c940e
Dustin Miller <dmiller903@gmail.com>
2020-04-10 08:08:01 -0600

    remove some queries

--- scripts/gemini_query.py
+++ scripts/gemini_query.py
@@ -20,83 +20,37 @@ outputName = args.output_file_prefix
 med = "'MED'"
 high = "'HIGH'"
 
-# CH Variants with no filters
-os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
-{inputDatabase} \
-> {outputName}_ch_no_filter.tsv')
-
-# Filter CH Variants with minor allele frequency <= 0.005 and cadd >= 20
+# Filter CH Variants with minor allele frequency <= 0.005 and cadd >= 20 (Stringent)
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
 {inputDatabase} \
 > {outputName}_ch_impactHM_aaf005_cadd20.tsv')
 
-# Filter CH Variants with minor allele frequency <= 0.01 and cadd >= 20
-os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=20)" \
-{inputDatabase} \
-> {outputName}_ch_impactHM_aaf01_cadd20.tsv')
-
-# Filter CH Variants with no regard for minor allele frequency and cadd >= 20
-os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=20)" \
-{inputDatabase} \
-> {outputName}_ch_impactHM_cadd20.tsv')
-
-# Filter CH Variants with minor allele frequency <= 0.005 and cadd >= 15
-os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=15)" \
-{inputDatabase} \
-> {outputName}_ch_impactHM_aaf005_cadd15.tsv')
-
-# Filter CH Variants with minor allele frequency <= 0.01 and cadd >= 15
+# Filter CH Variants with minor allele frequency <= 0.01 and cadd >= 15 (Less Stringent)
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=15)" \
 {inputDatabase} \
 > {outputName}_ch_impactHM_aaf01_cadd15.tsv')
 
-# Filter CH Variants with no regard for minor allele frequency and cadd >= 15
+# Filter CH Variants with no regard for minor allele frequency and cadd >= 15 (No MAF)
 os.system(f'gemini comp_hets --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=15)" \
 {inputDatabase} \
 > {outputName}_ch_impactHM_cadd15.tsv')
 
-# de novo variants with no filters
-os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
-{inputDatabase} \
-> {outputName}_de_novo_no_filter.tsv')
-
-# Filter for de novo variants with minor allele frequncey <= 0.005 and cadd >= 20
+# Filter for de novo variants with minor allele frequncey <= 0.005 and cadd >= 20 (Stringent)
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=20)" \
 {inputDatabase} \
 > {outputName}_de_novo_impactHM_aaf005_cadd20.tsv')
 
-# Filter for de novo variants with minor allele frequncey <= 0.01 and cadd >= 20
-os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=20)" \
-{inputDatabase} \
-> {outputName}_de_novo_impactHM_aaf01_cadd20.tsv')
-
-# Filter for de novo variants with no regard for minor allele frequency and cadd >= 20
-os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=20)" \
-{inputDatabase} \
-> {outputName}_de_novo_impactHM_cadd20.tsv')
-
-# Filter for de novo variants with minor allele frequncey <= 0.005 and cadd >= 15
-os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
---filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.005 and cadd_scaled >=15)" \
-{inputDatabase} \
-> {outputName}_de_novo_impactHM_aaf005_cadd15.tsv')
-
-# Filter for de novo variants with minor allele frequncey <= 0.01 and cadd >= 15
+# Filter for de novo variants with minor allele frequncey <= 0.01 and cadd >= 15 (Less Stringent)
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and aaf_1kg_all <= 0.01 and cadd_scaled >=15)" \
 {inputDatabase} \
 > {outputName}_de_novo_impactHM_aaf01_cadd15.tsv')
 
-# Filter for de novo variants with no regard for minor allele frequency and cadd >= 15
+# Filter for de novo variants with no regard for minor allele frequency and cadd >= 15 (No MAF)
 os.system(f'gemini de_novo --columns "chrom, start, vcf_id, ref, alt, gene, impact_severity, aaf_1kg_all, cadd_scaled, impact, biotype" \
 --filter "(impact_severity = {high} or is_lof = 1) or (impact_severity = {med} and cadd_scaled >=15)" \
 {inputDatabase} \
