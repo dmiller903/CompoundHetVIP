5fdef1bccf8d9eb702d6183c86b596be8ec9b1cf
Dustin Miller <dmiller903@gmail.com>
2020-05-06 14:06:09 -0600

    remove print statement

--- scripts/concat_merge_phased_vcf.py
+++ scripts/concat_merge_phased_vcf.py
@@ -79,7 +79,6 @@ for key, value in filesToConcat.items():
         os.system(f"gzip -d {file}")
         os.system(f"bgzip -f {file.rstrip('.gz')}")
         os.system(f"tabix -fp vcf {file}")
-        print(file)
     tempOutput = f"/tmp/{key}_phased_combined.vcf.gz"
     files = " ".join(value)
     os.system(f"bcftools concat {files} -o {tempOutput} -O z")
