f8549d1f0ae98a80a1a42e1a553d5bd35713e297
Dustin Miller <dmiller903@gmail.com>
2020-09-09 11:20:09 -0600

    update how .fam is split

--- scripts/identify_homAlt_variants.py
+++ scripts/identify_homAlt_variants.py
@@ -108,7 +108,7 @@ if familyFile is not None:
     familyDict = {}
     with open(familyFile) as familyFile:
         for line in familyFile:
-            lineList = line.rstrip("\n").split("\t")
+            lineList = line.rstrip("\n").split()
             if lineList[-1] is "2":
                 parentDict[f"gts.{lineList[2]}"] = f"gts.{lineList[1]}"
                 parentDict[f"gts.{lineList[3]}"] = f"gts.{lineList[1]}"
@@ -215,7 +215,7 @@ with open(geminiTsv) as geminiFile, open(outputFile, "w") as outputFile:
         start, gene, ref, alt, impact, cadd, maf, lof, exonic = getLineInfo(lineList)
         for sampleIndex in sampleIndexes:
             sample = headerList[sampleIndex]
-            if gene in homAltPositionDict[sample] and start in homAltPositionDict[sample][gene] and len(homAltPositionDict[sample][gene]) >= 2:
+            if sample in homAltPositionDict and gene in homAltPositionDict[sample] and start in homAltPositionDict[sample][gene] and len(homAltPositionDict[sample][gene]) >= 2:
                 genotype = lineList[sampleIndex]
                 numericGenotype = getNumericGenotype(genotype, ref, alt)
                 if "." not in numericGenotype and numericGenotype == "1|1":
